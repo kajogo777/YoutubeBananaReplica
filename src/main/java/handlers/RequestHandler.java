ded3136923c58c6a77c4e0a52d49abccc0bd9610
George <kajogo777@gmail.com>
2018-02-22 18:39:24 +0200

    Initial commit handlers & server

---
+++ src/main/java/handlers/RequestHandler.java
@@ -0,0 +1,67 @@
+package handlers;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.AttributeKey;
+
+import java.util.UUID;
+
+
+public class RequestHandler extends ChannelInboundHandlerAdapter {
+
+    String requestId;
+
+    private String parseToJson(final FullHttpRequest req){
+        String result = "canary";
+
+        System.out.println(req.uri());
+        System.out.println(req.method());
+        System.out.println(req.content());
+        System.out.println(req.headers());
+
+//        {
+//            "type": //CREATE or READ or UPDATE or DELETE],
+//            "parameters": {
+//                url arguments (read) of json (create, update, delete)
+//            }
+//        }
+
+        return result;
+    }
+
+    private String getServiceName(final FullHttpRequest req){
+        String result = "APP";
+
+        return result;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception{
+        final FullHttpRequest req = (FullHttpRequest) msg;
+
+        String service = getServiceName(req);
+        String data = parseToJson(req);
+
+        requestId = UUID.randomUUID().toString();
+
+        ctx.channel().attr(AttributeKey.valueOf("SERVICE")).set(service);
+        ctx.channel().attr(AttributeKey.valueOf("REQUESTID")).set(requestId);
+        //ctx.channel().attr(AttributeKey.newInstance("DATA")).set(data);
+
+        ctx.fireChannelRead(data); //maybe send json
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception{
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
+
